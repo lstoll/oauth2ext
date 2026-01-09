e9509c452acaaf531f3a732989114f5beaac9449
Lincoln Stoll <me@lds.li>
2026-01-09 01:20:03 +0100

    clitoken: Add support for getting a DPoP signer
    First place I want to use DPoP is CLI tool refresh tokens, so expose a way to
    get a signer without exposing the internal keychain package. This is done in a
    similar way to the credential cache.
    
    Likely going to re-work the BestSigner/Cache setup at some point to allow for
    easier external implemetations, but for now this will do.

---
+++ clitoken/signer_darwin.go
@@ -0,0 +1,73 @@
+//go:build darwin && cgo
+
+package clitoken
+
+import (
+	"crypto"
+	"errors"
+	"io"
+	"runtime"
+
+	"lds.li/oauth2ext/internal/keychain"
+)
+
+const sepSignerLabel = "oauth2ext-cli-sep"
+
+func init() {
+	platformSigners = append(platformSigners, newDarwinSEPSigner)
+}
+
+type darwinSEPSigner struct {
+	identity *keychain.CTKIdentity
+	signer   crypto.Signer
+}
+
+var _ crypto.Signer = &darwinSEPSigner{}
+
+func newDarwinSEPSigner() (crypto.Signer, error) {
+	identity, err := keychain.GetCTKIdentity(sepSignerLabel, nil)
+	if err != nil {
+		var kcErr *keychain.Error
+		if !errors.As(err, &kcErr) || kcErr.Code != keychain.KeychainErrorCodeItemNotFound {
+			return nil, err
+		}
+
+		// Identity doesn't exist, create it
+		_, err = keychain.CreateCTKIdentity(sepSignerLabel, keychain.CTKKeyTypeP256)
+		if err != nil {
+			return nil, err
+		}
+
+		// Get the identity with signing capability
+		identity, err = keychain.GetCTKIdentity(sepSignerLabel, nil)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	// Get the signer from the identity
+	signer, err := identity.Signer()
+	if err != nil {
+		identity.Close()
+		return nil, err
+	}
+
+	dss := &darwinSEPSigner{
+		identity: identity,
+		signer:   signer,
+	}
+
+	runtime.AddCleanup(dss, func(identity *keychain.CTKIdentity) {
+		identity.Close()
+	}, identity)
+
+	return dss, nil
+}
+
+func (d *darwinSEPSigner) Public() crypto.PublicKey {
+	return d.signer.Public()
+}
+
+func (d *darwinSEPSigner) Sign(rand io.Reader, digest []byte, opts crypto.SignerOpts) (signature []byte, err error) {
+	return d.signer.Sign(rand, digest, opts)
+}
